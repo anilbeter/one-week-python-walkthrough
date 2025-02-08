ab3105a4263da2c3e75a8255b7db9c3701f95ec7
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2025-02-08 15:14:49 +0300

    f string

--- main.py
+++ main.py
@@ -1,12 +1,2 @@
-age = input("Soo how old are you?? ")
-print(type(age))
-# str
-# even if you write a number like 23, it will return 'str' type.
-# so if you want to mathematical operations depens on the age, you have to type conversion
-
-age_new = int(input("So how old are you??"))
-print(type(age_new))
-# int
-
-print(type(str(29)))
-# str
+print(f"there are {24*60*60} seconds in a day")
+# there are 86400 seconds in a day
