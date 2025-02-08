eb98af51497ab6b811242fc179a2578b4bfcfa85
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2025-02-08 15:17:03 +0300

    upper and lower

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
-print(f"there are {24*60*60} seconds in a day")
-# there are 86400 seconds in a day
+msg = "hello world"
+print(msg.capitalize())
+# Hello world
+print(msg.upper())
+# HELLO WORLD
+print("ANIL".lower())
+# anil
