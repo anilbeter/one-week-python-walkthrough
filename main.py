2c079bea9bbed3f5a2de2c9f67ff4a0251bc029e
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2025-02-08 14:33:31 +0300

    string variables

--- main.py
+++ main.py
@@ -1,26 +1,3 @@
-score = 500
-print(score)  # 500
-score *= 3
-print(score)  # 1500
-score = 37
-print(score)  # 37
-score = score + 1
-print(score)  # 38
-
-
-player_1 = 0
-player_one = 0
-PI = 3.14159
-
-
-age = 23
-age += 1
-print(age)  # 24
-age //= 2
-print(age)  # 12
-age *= 2
-print(age)  # 24
-
-num = 2
-num **= 3
-print(num)  # 8
+age = 85
+age = "eighty-five"
+print(age)  # eighty-five
