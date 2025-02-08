a867587988a2c1490c89a62fe7b26cca2f79325e
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2025-02-08 15:10:20 +0300

    type casting

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
-name = input("What is your name?? ")
-print("Hello " + name[0].upper() + name[1:])
+age = input("Soo how old are you?? ")
+print(type(age))
+# str
+# even if you write a number like 23, it will return 'str' type.
+# so if you want to mathematical operations depens on the age, you have to type conversion
+
+age_new = int(input("So how old are you??"))
+print(type(age_new))
+# int
+
+print(type(str(29)))
+# str
