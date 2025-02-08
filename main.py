ccaa55587de36c2c32c52f29a5b8f5a4b26d5053
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2025-02-08 13:53:59 +0300

    python numbers & operators

---
+++ main.py
@@ -0,0 +1,21 @@
+print(12)  # 12
+print(12 == "12")  # False
+print(type(23))  # int
+print(type("Anil"))  # str
+print(type("Anil") == str)  # True
+print(type(1.243))  # float
+
+print(1_000)  # 1000
+print(0.8)  # 0.8
+print(1_000.55)  # 1000.55
+print(101414329432458534543.0000)  # 1.0141432943245853e+20
+
+print(1 + 4)  # 3
+print(4 - 1.0)  # 3.0
+print(1 / 3)  # 0.33333
+print(6 * 3 - 10)  # 8
+
+print(2**4)  # 16
+print(10 // 3)  # 3
+print(-10 // 3)  # - 4
+print(11 % 5)  # 1
