5c60dca05d9c5184bf5f974efe531f9645ef5ed0
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2025-02-09 11:40:07 +0300

    boolean & comparison

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
-email = "     aNiL.hehE@gmaIl.coM   "
-correct_email = email.strip().lower()
-print(correct_email)
-# anil.hehe@gmail.com
+if "anil" and 23:
+    print("oof drrrrmmmm ooNNnnN")
+# oof drrrrmmmm ooNNnnN
+
+print(4.0 == 4)
+# True
+
+print("4" == 4)
+# False
+
+if "":
+    print("truthy")
+else:
+    print("falsy")
+# falsy
