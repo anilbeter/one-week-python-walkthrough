daea01ea08af0450260b109bffafb8cc245daf02
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2025-02-09 11:47:27 +0300

    in operator

--- main.py
+++ main.py
@@ -1,15 +1,2 @@
-if "anil" and 23:
-    print("oof drrrrmmmm ooNNnnN")
-# oof drrrrmmmm ooNNnnN
-
-print(4.0 == 4)
+print("a" in "anil")
 # True
-
-print("4" == 4)
-# False
-
-if "":
-    print("truthy")
-else:
-    print("falsy")
-# falsy
